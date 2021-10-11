58b48312250d5ecf12f7ee948d8d6f4152b3c95b
ronaldvic <ronaldblancobalboa@gmail.com>
2021-10-11 12:12:45 -0400

    lectura y parseo del body añadido

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,73 +1,145 @@
 const { response } = require('express');
 
 const usuariosSalonesGet = (req,res=response) => {
-    res.send('Solicitud GET a salones, controlador');
+    res.json({
+        msg: 'Solicitud GET a salones, controlador',
+    });
 }
 const usuariosSalonesPut = (req,res=response) => {
-    res.send('Solicitud PUT a salones, controlador');
+    const {golden , platinum} = req.body;
+    res.json({
+        msg: 'Solicitud PUT a salones, controlador',
+        golden, platinum 
+    });
 }
 const usuariosSalonesPost = (req,res=response) => {
-    res.send('Solicitud POST a salones, controlador');
+    const {golden , platinum} = req.body;
+    res.json({
+        msg: 'Solicitud POST a salones, controlador',
+        golden, platinum 
+    });   
 }
 const usuariosSalonesDelete = (req,res=response) => {
-    res.send('Solicitud DELETE a salones, controlador')
+    const {golden , platinum} = req.body;
+    res.json({
+        msg: 'Solicitud DELETE a salones, controlador',
+        golden, platinum 
+    });
 }
 
 
 const usuariosComidaGet = (req,res=response) => {
-    res.send('Solicitud GET a comida, controlador');
+    res.json({
+            msg: 'Solicitud GET a comida, controlador',
+    });
 }
 const usuariosComidaPut = (req,res=response) => {
-    res.send('Solicitud PUT a comida, controlador');
+    const { golden, platinum, externo } = req.body;
+    res.json({
+            msg: 'Solicitud PUT a comida, controlador',
+            golden, platinum, externo
+    });
 }
 const usuariosComidaPost= (req,res=response) => {
-    res.send('Solicitud POST a comida, controlador');
+    const { golden, platinum, externo } = req.body;
+    res.json({
+            msg: 'Solicitud POST a comida, controlador',
+            golden, platinum, externo
+    });
 }
 const usuariosComidaDelete = (req,res=response) => {
-    res.send('Solicitud DELETE a comida, controlador');
+    const { golden, platinum, externo } = req.body;
+    res.json({
+            msg: 'Solicitud DELETE a comida, controlador',
+            golden, platinum, externo
+    });
 }
 
 
 const usuariosMusicaGet = (req,res=response) => {
-    res.send('Solicitud GET a musica, controlador');
+    res.json({
+            msg: 'Solicitud GET a musica, controlador',
+    });
 }
 const usuariosMusicaPut = (req,res=response) => {
-    res.send('Solicitud PUT a musica, controlador');
+    const { golden, platinum, externo } = req.body;
+    res.json({
+            msg: 'Solicitud PUT a musica, controlador',
+            golden, platinum, externo
+    });
 }
 const usuariosMusicaPost = (req,res=response) => {
-    res.send('Solicitud POST a musica, controlador');
+    const { golden, platinum, externo } = req.body;
+    res.json({
+            msg: 'Solicitud POST a musica, controlador',
+            golden, platinum, externo
+    });
 }
 const usuariosMusicaDelete = (req,res=response) => {
-    res.send('Solicitud DELETE a musica, controlador');
+    const { golden, platinum, externo } = req.body;
+    res.json({
+            msg: 'Solicitud DELETE a musica, controlador',
+            golden, platinum, externo
+    });
 }
 
 
 const usuariosBartenderGet = (req,res=response) => {
-    res.send('Solicitud GET a bartender, controlador');
+    res.json({
+            msg: 'Solicitud GET a bartender, controlador',
+    });
 }
 const usuariosBartenderPut = (req,res=response) => {
-    res.send('Solicitud PUT a bartender, controlador');
+    const { golden, platinum, externo } = req.body;
+    res.json({
+            msg: 'Solicitud PUT a bartender, controlador',
+            golden, platinum, externo
+    });
 }
 const usuariosBartenderPost = (req,res=response) => {
-    res.send('Solicitud POST a bartender, controlador');
+    const { golden, platinum, externo } = req.body;
+    res.json({
+            msg: 'Solicitud POST a bartender, controlador',
+            golden, platinum, externo
+    });
 }
 const usuariosBartenderDelete = (req,res=response) => {
-    res.send('Solicitud DELETE a bartender, controlador');
+    const { golden, platinum, externo } = req.body;
+    res.json({
+            msg: 'Solicitud DELETE a bartender, controlador',
+            golden, platinum, externo
+    });
 }
 
 
 const usuariosDecoracionesGet = (req,res=response) => {
-    res.send('Solicitud GET a decoraciones, controlador');
+    res.json({
+            msg: 'Solicitud GET a decoraciones, controlador',
+    });
 }
 const usuariosDecoracionesPut = (req,res=response) => {
-    res.send('Solicitud PUT a decoraciones, controlador');
+    const { golden, platinum, externo } = req.body;
+    res.json({
+            msg: 'Solicitud PUT a decoraciones, controlador',
+            golden, platinum, externo
+    });
 }
 const usuariosDecoracionesPost = (req,res=response) => {
-    res.send('Solicitud POST a decoraciones, controlador');
+    const { golden, platinum, externo } = req.body;
+    res.json({
+            msg: 'Solicitud POST a decoraciones, controlador',
+            golden, platinum, externo
+    });
 }
 const usuariosDecoracionesDelete = (req,res=response) => {
-    res.send('Solicitud DELETE a decoraciones, controlador');
+    const { golden, platinum, externo } = req.body;
+    res.json({
+            msg: 'Solicitud DELETE a decoraciones, controlador',
+            golden, platinum, externo
+    });
 }
+
+
 
 module.exports = {
     usuariosSalonesGet,
